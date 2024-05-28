cf512765775f7395fe72592737d5a6d5d6e363c3
ayoraind <afolayanayorinde@gmail.com>
2024-05-28 09:47:29 +0200

    fixed a minor mistake in the lines selected for awk print operation

--- bin/transpose_cramino.sh
+++ bin/transpose_cramino.sh
@@ -2,17 +2,21 @@
 
 FILE=${1}
 
-echo "Filename"$'\t'"number_of_reads"$'\t'"yield_GB"$'\t'"mean_coverage"$'\t'"N50"$'\t'"median_length"$'\t'"mean_length"$'\t'"median_gap_excluded_id"$'\t'"mean_gap_excluded_id" > ${FILE}.transposed.craminoStats.txt
+echo "Filename"$'\t'"number_of_alignments"$'\t'"percentage_from_total_reads"$'\t'"yield_GB"$'\t'"mean_coverage"$'\t'"N50"$'\t'"N75"$'\t'"median_length"$'\t'"mean_length"$'\t'"median_identity"$'\t'"mean_identity"$'\t'"modal_identity" > ${FILE}.transposed.craminoStats.txt
 
 
-number_of_reads=$(cat ${FILE}.cramino.txt | awk 'NR == 2 {print $4}')
-yield_GB=$(cat ${FILE}.cramino.txt | awk 'NR == 3 {print $3}')
-mean_coverage=$(cat ${FILE}.cramino.txt | awk 'NR == 4 {print $3}')
-N50=$(cat ${FILE}.cramino.txt | awk 'NR == 5 {print $2}')
-median_length=$(cat ${FILE}.cramino.txt | awk 'NR == 6 {print $3}')
-mean_length=$(cat ${FILE}.cramino.txt | awk 'NR == 7 {print $3}')
-median_gap_excluded_id=$(cat ${FILE}.cramino.txt | awk 'NR == 8 {print $3}')
-mean_gap_excluded_id=$(cat ${FILE}.cramino.txt | awk 'NR == 9 {print $3}')
+number_of_alignments=$(cat ${FILE}.cramino.txt | awk 'NR == 2 {print $4}')
+percentage_from_total_reads=$(cat ${FILE}.cramino.txt | awk 'NR == 3 {print $5}')
+yield_GB=$(cat ${FILE}.cramino.txt | awk 'NR == 4 {print $3}')
+mean_coverage=$(cat ${FILE}.cramino.txt | awk 'NR == 5 {print $3}')
+yield_GB_gt_25kb=$(cat ${FILE}.cramino.txt | awk 'NR == 6 {print $4}')
+N50=$(cat ${FILE}.cramino.txt | awk 'NR == 7 {print $2}')
+N75=$(cat ${FILE}.cramino.txt | awk 'NR == 8 {print $2}')
+median_length=$(cat ${FILE}.cramino.txt | awk 'NR == 9 {print $3}')
+mean_length=$(cat ${FILE}.cramino.txt | awk 'NR == 10 {print $3}')
+median_identity=$(cat ${FILE}.cramino.txt | awk 'NR == 11 {print $3}')
+mean_identity=$(cat ${FILE}.cramino.txt | awk 'NR == 12 {print $3}')
+modal_identity=$(cat ${FILE}.cramino.txt | awk 'NR == 13 {print $3}')
 #num=number, perc=percentage, mb=megabases
 
-printf "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" $FILE $number_of_reads $yield_GB $mean_coverage $N50 $median_length $mean_length $median_gap_excluded_id $mean_gap_excluded_id  >> ${FILE}.transposed.craminoStats.txt
+printf "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" $FILE $number_of_alignments $percentage_from_total_reads $yield_GB $mean_coverage $yield_GB_gt_25kb $N50 $N75 $median_length $mean_length $median_identity $mean_identity $modal_identity  >> ${FILE}.transposed.craminoStats.txt
